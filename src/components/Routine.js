37b468234f08658bd854407c12b825c8654e94de
Anish Kumar <anishbishnoi127@gmail.com>
2022-10-08 01:42:06 +0530

    Routine Track

---
+++ src/components/Routine.js
@@ -0,0 +1,27 @@
+import { addDoc, collection } from 'firebase/firestore';
+import React ,{useState} from 'react'
+import { db } from '../config/firebase';
+
+const Routine = () => {
+const [title,setTitle] = useState("");
+
+const handleSubmit= async(e)=>{
+e.preventDefault();
+if(title!=""){
+    await addDoc(collection(db,"routine"),{
+        title,
+        completed:false
+    });
+    setTitle("");
+}
+}
+  return (
+    <form onSubmit={handleSubmit}>
+        <input type ={'text'} placeholder="Routine Task" value={title}
+        onChange={(e)=>setTitle(e.target.value)}/>
+        <button>+</button>
+    </form>
+  )
+}
+
+export default Routine
---
+++ src/components/RoutineList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const RoutineLIst = () => {
+  return (
+   <div className="routine">
+        <input type="text" className="list" value ={"welcome"} />
+        <div>
+            <button className="button-complete"></button>
+        </div>
+   </div>
+  )
+}
+
+export default RoutineLIst
