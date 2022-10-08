941543508f257c4b552fa2266f802d4415f32246
Anish Kumar <anishbishnoi127@gmail.com>
2022-10-09 00:09:09 +0530

    routine ui changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
+import { collection, onSnapshot, query, QuerySnapshot } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Routine from './components/Routine';
 import RoutineList from './components/RoutineList';
 function App() {
+  const [routines,setRoutines]  = useState([]);
+  useEffect(()=>{
+    const que = query(collection(db,"routine"));
+    const unsub = onSnapshot(que,(QuerySnapshot)=>{
+      let routineArray = [];
+      QuerySnapshot.forEach((doc)=>{
+        routineArray.push({...doc.data(),id:doc.id});
+      });
+      setRoutines(routineArray);
+    })
+    return()=>unsub();
+  })
   return (
     <div className="App">
    {/* <h1> mahadev </h1> */}
    <div className="title"><h1>Routine Track</h1></div>
    <div><Routine/></div>
-   <div><RoutineList/></div>
+   <div className="routine-container">
+    {
+      routines.map((item)=>{
+        <RoutineList key={routine.id} toggleComplete={toggleComplete} handleDelete={handleDelete} handleEdit={handleEdit}/>
+      })
+    }
+   </div>
+
     </div>
 
   );
